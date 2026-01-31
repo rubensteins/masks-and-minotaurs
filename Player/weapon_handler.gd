71a2f71fb42e628eda778b315ba94424112773a8
Ruben Steins <rubensteins@gmail.com>
2026-01-31 21:07:43 +0100

    Remove janky meshes, added block instead of masks

--- Player/weapon_handler.gd
+++ Player/weapon_handler.gd
@@ -14,17 +14,39 @@ func _process(_delta) -> void:
 			equip(0)
 		else: 
 			unequip(0)
+			
+	if Input.is_action_just_pressed("mask_2") and not animation_player.is_playing():
+		if not is_mask_on:
+			equip(1)
+		else: 
+			unequip(1)
+
+	if Input.is_action_just_pressed("mask_3") and not animation_player.is_playing():
+		if not is_mask_on:
+			equip(2)
+		else: 
+			unequip(2)
 
 func equip(new_mask_index : int) -> void:
 	masks[new_mask_index].visible = true
 	masks[new_mask_index].set_process(true)
 	active_mask = new_mask_index
-	animation_player.play("put_on_ghost_mask")
+	if new_mask_index == 0:
+		animation_player.play("mask_2")
+	if new_mask_index == 1:
+		animation_player.play("mask_3")
+	if new_mask_index == 2:
+		animation_player.play("mask_4")
 	is_mask_on = true
 
 func unequip(mask_index) -> void:
 	is_mask_on = false
-	animation_player.play_backwards("put_on_ghost_mask")
+	if mask_index == 0:
+		animation_player.play_backwards("mask_2")
+	if mask_index == 1:
+		animation_player.play_backwards("mask_3")
+	if mask_index == 2:
+		animation_player.play_backwards("mask_4")
 
 func hide_the_mask() -> void:
 	masks[active_mask].visible = false
