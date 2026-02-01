7b11a26db8a222ac9ccffe47b00ad82aae0ef3a5
Ruben Steins <rubensteins@gmail.com>
2026-02-01 15:16:47 +0100

    Controller Support

--- UI/game_over_screen.gd
+++ UI/game_over_screen.gd
@@ -7,6 +7,12 @@ extends Control
 
 @onready var game_over_label: Label = %GameOverLabel
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("mask_1"):
+		_on_restart_button_pressed()
+	if Input.is_action_just_pressed("quit"):
+		_on_quit_button_pressed()
+
 # Called when the node enters the scene tree for the first time.
 func game_over(hasWon: bool, message: String) -> void:
 	get_tree().paused = true
@@ -18,7 +24,6 @@ func game_over(hasWon: bool, message: String) -> void:
 	visible = true	
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
-
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
--- UI/title_screen.gd
+++ UI/title_screen.gd
@@ -8,3 +8,9 @@ func _on_restart_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("mask_1"):
+		_on_restart_button_pressed()
+	if Input.is_action_just_pressed("quit"):
+		_on_quit_button_pressed()
